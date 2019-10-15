267e093ea6787d46e8cb196b952a58bf92d504ef
남선웅 <nsw1107@naver.com>
2019-10-15 13:38:34 +0900

    ~update

--- form/urls.py
+++ form/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from movies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/',include('movies.urls'))
+    path('movies/',include('movies.urls')),
+    path('',views.index)
 ]
--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name="movies"
 urlpatterns = [
-    path('',views.index, name="index")
+    path('',views.index, name="index"),
+    path('create/',views.create, name="create"),
+    path('<int:id>/detail/',views.detail, name="detail"),
+    path('<int:id>/delete/',views.delete, name="delete"),
+    path('<int:id>/update/',views.update, name="update"),
 ]
